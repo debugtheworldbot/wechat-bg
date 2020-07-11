2af436e54fe138fc180956dc4583890c12f7615f
debugtheworldbot <istiancz@gmail.com>
2020-07-11 13:28:42 +0800

    sync

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,96 @@
-import React, {useState} from "react";
+import React, { useState} from 'react';
+import styled from 'styled-components';
 
 
 
+const Ul=styled.ul`
+  cursor: pointer;
+  font-size: 20px;
+`
 
-export default function App() {
+
+export default function App(){
+
     const [tag, setTag] = useState<string[]>([])
-    const [content,setContent]=useState<string>('')
+    const [content, setContent] = useState<string>('')
     const [urls, setUrl] = useState<string[]>([])
-    const [link,setLink]=useState<string>('')
+    const [link, setLink] = useState<string>('')
+    const [level1, setLevel1] = useState(-1)
+    const [toggle,setToggle]=useState(false)
 
 
-    const arr={buttons:[
-        {name: "扫码",
-        sub_button: [
+    const arr = {
+        buttons: [
             {
-                name: "扫码带提示",
-                url: "https://baidu.com",
-                sub_button: []
-            },
-            {
-                name: "扫码推事件",
-                url: "https://google.com",
-                sub_button: []
-            }
-        ]},{
-            name: "发图",
-            sub_button: [
-                {
-                    name: "系统拍照发图",
-                    url: "https://bilibili.com",
-                    sub_button: []
-                },
-                {
-                    name: "拍照或者相册发图",
-                    url: "https://baidu.com",
-                    sub_button: []
-                },
-                {
-                    name: "微信相册发图",
-                    url: "https://https://github.com",
-                    sub_button: []
-                }
-            ]},{
-            name: '帮助',
-            url: 'https://baidu.com'
-        }]}
+                name: "扫码", subButtons: [
+                    {
+                        name: "扫码带提示",
+                        url: "https://baidu.com",
+                        type: "view",
+                    },
+                    {
+                        name: "扫码推事件",
+                        url: "https://google.com",
+                        type: "view",
+                    }
+                ]
+            }, {
+                name: "发图", subButtons: [
+                    {
+                        name: "系统拍照发图",
+                        url: "https://bilibili.com",
+                        type: "view",
 
+                    },
+                    {
+                        name: "拍照或者相册发图",
+                        url: "https://baidu.com",
+                        type: "view",
+
+                    },
+                    {
+                        name: "微信相册发图",
+                        url: "https://github.com",
+                        type: "view",
+                    }
+                ]
+            }, {
+                name: '帮助',
+                url: 'https://baidu.com'
+            }]
+    }
+    const [Arr, setArr] = useState(arr)
 
     let myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    let raw = JSON.stringify(arr);
-    fetch("http://127.0.0.1:7001/wechat", {method:'POST',headers:myHeaders,body:raw})
+    let raw = JSON.stringify(Arr);
+    fetch("https://test-api-wecare.medtreehealth.com/v2/api-docs?group=wechat", {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw
+    })
         .then(response => response.text())
-        .then(result => console.log(result))
         .catch(error => console.log('error', error));
 
 
-    // fetch(request)
-    //     .then(response => {
-    //         if (response.status === 200) {
-    //             console.log('success')
-    //         } else {
-    //             throw new Error('Something went wrong on api server!');
-    //         }
-    //     })
-    //     .then(data => {
-    //         console.log(data)
-    //         // ...
-    //     }).catch(error => {
-    //     console.error(error);
-    // });
-
-    function show(title: string) {
+    function show(title: string, index: number) {
         setContent(title)
+        setLevel1(index)
         // 获取二级菜单名
-        const text = arr.buttons.filter(item => item.name === title)
-        const detail = text[0].sub_button
+        const text = Arr.buttons.filter(item => item.name === title)
+        const detail = text[0].subButtons
         if (detail) {
             const data = [detail.map(item => item.name), detail.map(item => item.url)]
             setTag(data[0])
             setUrl(data[1])
         }
-        if(text[0].url){
+        if (text[0].url) {
             setLink(text[0].url)
-            window.open(text[0].url)
-        }else {
+        } else {
             setLink('')
         }
     }
 
-    function goToLink(item: string) {
-        setContent(item)
-        let index = tag.indexOf(item)
-        setLink(urls[index])
-        window.open(urls[index])
-    }
-
-    function autoResponse(type: 'subscribe' | 'message') {
-        if (type === 'subscribe') {
-            console.log('thanks for following me.')
-        }
-        if (type === 'message') {
-            console.log(`we've received your message!`)
-        }
-    }
     // function changeContent(content:string) {
     //     const text= arr.filter(item=>item.name===content)
     //     console.log(text)
@@ -117,9 +104,6 @@ export default function App() {
     //         }
     //     }
     // }
-    // function changeLink(link:string) {
-    //     console.log(link)
-    // }
     // function addList() {
     //     const text=window.prompt('输入添加的内容')
     //     if(text){
@@ -131,27 +115,57 @@ export default function App() {
     //     }
     // }
 
-    const title=arr.buttons.map(item => <li key={item.name} onClick={() => show(item.name)}>{item.name}</li>)
-    const list = tag.map(item => <li key={item} onClick={() => goToLink(item)}>{item}</li>)
+
+    function changeList(item: string, index: number) {
+        // @ts-ignore
+        Arr.buttons[level1].subButtons[index].name = window.prompt('change?') || Arr.buttons[level1].subButtons[index].name
+        setArr(Arr)
+        show(Arr.buttons[level1].name,level1)
+    }
+    function changeUrl(url:string,index:number) {
+        // @ts-ignore
+        Arr.buttons[level1].subButtons[index].url = window.prompt('change?') || Arr.buttons[level1].subButtons[index].url
+        setArr(Arr)
+        show(Arr.buttons[level1].name,level1)
+    }
+
+    function addTitle(name:string,link:string) {
+
+        // @ts-ignore
+        setArr({...Arr},{name:name, url: link})
+    }
+
+    const title = Arr.buttons.map((item, index) => <li key={item.name}
+                                                       onClick={() => show(item.name, index)}>{item.name}</li>)
+    const list = tag.map((item, index) => <li onClick={() => {
+        changeList(item, index)
+    }}>{item}</li>)
+    const url=urls.map((url, index) => <li onClick={() => {
+        changeUrl(url, index)
+    }}>{url}</li>)
+
 
     return (
-        <div className="App">
             <div>
-                <ul>{title}</ul>
-                <ul>
-                    {list}
-                </ul>
-            </div>
-            <div>
-                <button onClick={() => autoResponse('subscribe')}>订阅</button>
-                <button onClick={() => autoResponse('message')}>发消息</button>
-            </div>
-            {/*<div>*/}
-            {/*    编辑*/}
-            {/*    <div onClick={()=>changeContent(content)}>{content}</div>*/}
-            {/*    <div onClick={()=>changeLink(link)}>{link}</div>*/}
-            {/*</div>*/}
+                <div>
+                    <Ul>
+                        <h2>主菜单</h2>
+                        {title}
+                        {link}
+                        {/* eslint-disable-next-line no-restricted-globals */}
+                        <div onClick={()=>{addTitle(name,link)}}>{Arr.buttons.length===1?'':`+`}</div>
 
-        </div>
+                    </Ul>
+                    <Ul>
+                        <h1>子菜单</h1>
+                        {list}
+                    </Ul>
+                    <Ul>
+
+                        {url}
+                    </Ul>
+                </div>
+            </div>
     );
-}
+};
+
